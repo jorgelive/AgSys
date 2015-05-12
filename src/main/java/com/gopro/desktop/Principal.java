7bf08c7b30f23b25b31cd89c3befe20706166a72
Jorge Luis Gomez (desktop 1) <jgomezv@outlook.com>
2015-05-11 22:21:41 -0500

    Agregando acciones de guardado

--- src/main/java/com/gopro/desktop/Principal.java
+++ src/main/java/com/gopro/desktop/Principal.java
@@ -33,6 +33,7 @@ public class Principal extends javax.swing.JFrame {
         jMenuItem1 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -52,6 +53,14 @@ public class Principal extends javax.swing.JFrame {
             }
         });
         jMenu2.add(jMenuItem2);
+
+        jMenuItem3.setText("Contactos");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem3);
 
         jMenuBar1.add(jMenu2);
 
@@ -75,6 +84,11 @@ public class Principal extends javax.swing.JFrame {
         PaisJframe pais = new PaisJframe();
         pais.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        ContactoJframe contacto = new ContactoJframe();
+        contacto.setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -117,5 +131,6 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/main/java/com/gopro/model/ContactoModel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gopro.model;
+
+import com.gopro.entity.*;
+
+/**
+ *
+ * @author jgomez
+ */
+public class ContactoModel extends AbstractModel<Contacto> {
+    
+    public ContactoModel(){
+        super(Contacto.class);
+    }
+    
+}
